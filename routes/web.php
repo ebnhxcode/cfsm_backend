d6fca8bd5b5905f9a8f45a3476b7327f1419cc3e
Esteban Ramos Fernández <esteban.ramos.f@gmail.com>
2019-01-03 14:59:25 -0500

    update - deps

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,30 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::resource('calibres', 'CalibreController');
+Route::resource('categorias', 'CategoriaController');
+Route::resource('comunas', 'ComunaController');
+Route::resource('conceptos', 'ConceptoController');
+Route::resource('defectos', 'DefectoController');
+Route::resource('embalajes', 'EmbalajeController');
+Route::resource('estados_muestras', 'EstadoMuestraController');
+Route::resource('etiquetas', 'EtiquetaController');
+Route::resource('lotes', 'LoteController');
+Route::resource('muestras', 'MuestraController');
+Route::resource('muestras_defectos', 'MuestraDefectoController');
+Route::resource('notas', 'NotaController');
+Route::resource('producotres', 'ProductorController');
+Route::resource('provincias', 'ProvinciaController');
+Route::resource('regiones', 'RegionController');
+Route::resource('tolerancias', 'ToleranciaController');
+Route::resource('users', 'UsersController');
+Route::resource('variedades', 'VariedadController');
+Route::resource('zonas_defectos', 'ZonaDefectoController');
+
+
+
+
+
+
