799f1433a9da1d5ade3fe322b49ef246b24f6d3e
CELMEDIA\fcarrasco <fcarrasco@celmedia.com>
2019-01-11 12:58:08 -0300

    Commit

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -106,7 +106,7 @@
         <li class="nav-item">
           <a class="nav-link" href="{!!URL::to('/muestras')!!}">
             <i class="fas fa-fw fa-table"></i>
-            <span>Tables</span></a>
+            <span>Muestras</span></a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="{{ url('reportes') }}">
