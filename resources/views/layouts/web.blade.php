d9a07a1bcde667b89b633b7901f884829966c236
CELMEDIA\fcarrasco <fcarrasco@celmedia.com>
2019-01-11 10:59:40 -0300

    muestras

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -104,7 +104,7 @@
             <span>Graficos</span></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="tables.html">
+          <a class="nav-link" href="{!!URL::to('/muestras')!!}">
             <i class="fas fa-fw fa-table"></i>
             <span>Tables</span></a>
         </li>
